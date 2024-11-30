0fc3506c0c7a1474059737ac7fea10d7cf90ccba
ganesh8prasad1991 <ganesh8prasad1991@gmail.com>
2024-11-30 14:05:41 +0530

    Initial Commit

---
+++ StickyHeader/StickyHeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickyHeaderApp.swift
+//  StickyHeader
+//
+//  Created by Ganesh Prasad on 30/11/24.
+//
+
+import SwiftUI
+
+@main
+struct StickyHeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StickyHeaderTests/StickyHeaderTests.swift
@@ -0,0 +1,17 @@
+//
+//  StickyHeaderTests.swift
+//  StickyHeaderTests
+//
+//  Created by Ganesh Prasad on 30/11/24.
+//
+
+import Testing
+@testable import StickyHeader
+
+struct StickyHeaderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
